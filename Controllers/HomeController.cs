5cd0e8566e43f9a0fbe82d23b142a3bbfc149626
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2026-05-11 01:46:21 +0300

    Add dashboard charts - sales over time, top products, revenue by category

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using WebApplication2.Filters;
 using WebApplication2.Models.Data;
@@ -21,6 +23,67 @@ namespace WebApplication2.Controllers
                 .Sum(i => (decimal?)i.TotalAmount) ?? 0;
 
             return View();
+        }
+
+        // AJAX: Sales over last 30 days
+        public JsonResult GetSalesChart()
+        {
+            var thirtyDaysAgo = DateTime.Now.AddDays(-30);
+
+            var data = db.Invoices
+                .Where(i => i.Status == "Confirmed" && i.InvoiceDate >= thirtyDaysAgo)
+                .GroupBy(i => System.Data.Entity.DbFunctions.TruncateTime(i.InvoiceDate))
+                .Select(g => new
+                {
+                    date = g.Key,
+                    total = g.Sum(i => i.TotalAmount)
+                })
+                .OrderBy(g => g.date)
+                .ToList();
+
+            var labels = data.Select(d => d.date.Value.ToString("MM/dd")).ToList();
+            var values = data.Select(d => d.total).ToList();
+
+            return Json(new { labels, values }, JsonRequestBehavior.AllowGet);
+        }
+
+        // AJAX: Top 5 products by quantity sold
+        public JsonResult GetTopProducts()
+        {
+            var data = db.InvoiceItems
+                .GroupBy(i => i.ProductName)
+                .Select(g => new
+                {
+                    name = g.Key,
+                    qty = g.Sum(i => i.Quantity)
+                })
+                .OrderByDescending(g => g.qty)
+                .Take(5)
+                .ToList();
+
+            var labels = data.Select(d => d.name).ToList();
+            var values = data.Select(d => d.qty).ToList();
+
+            return Json(new { labels, values }, JsonRequestBehavior.AllowGet);
+        }
+
+        // AJAX: Revenue by category
+        public JsonResult GetRevenueByCategory()
+        {
+            var data = db.InvoiceItems
+                .GroupBy(i => i.Product.Category.NameEn)
+                .Select(g => new
+                {
+                    category = g.Key,
+                    revenue = g.Sum(i => i.LineTotal)
+                })
+                .OrderByDescending(g => g.revenue)
+                .ToList();
+
+            var labels = data.Select(d => d.category).ToList();
+            var values = data.Select(d => d.revenue).ToList();
+
+            return Json(new { labels, values }, JsonRequestBehavior.AllowGet);
         }
 
         protected override void Dispose(bool disposing)
